b406518c7cef7c9332994abc305b212f0cc22339
Subha Patra <56377135+subha-wp@users.noreply.github.com>
2025-01-20 15:36:14 +0530

    Create loading.tsx

---
+++ src/app/user/loading.tsx
@@ -0,0 +1,9 @@
+import AnimatedBrand from "@/components/HomeUi/AnimatedBrand";
+
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <AnimatedBrand />
+    </div>
+  );
+}
